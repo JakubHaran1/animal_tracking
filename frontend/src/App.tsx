afffc5d28dfd28edd318dabd3c1b0cf2597527c9
PawelSala <pawel.sala01@wp.pl>
2026-03-20 22:08:41 +0100

    feat(frontend): update page title and favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import { AppRouter } from "./router";
 
 function App() {
+  useEffect(() => {
+    const activeTitle = "Co tam misiu?";
+    const inactiveTitle = "Wróć do nas...";
+
+    const handleVisibilityChange = () => {
+      document.title = document.hidden ? inactiveTitle : activeTitle;
+    };
+
+    document.title = activeTitle;
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
+
   return <AppRouter />;
 }
 
