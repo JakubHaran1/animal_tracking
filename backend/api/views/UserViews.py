bf4ce27749738ce52805f0d6086cb484e32e5e6a
PawelSala <pawel.sala01@wp.pl>
2026-05-05 01:38:40 +0200

    backend: update API urls and user views

--- backend/api/views/UserViews.py
+++ backend/api/views/UserViews.py
@@ -1,22 +1,32 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from api.models import  User
 from api.serializers.ObservationSerializers import UserCreateSerializer, UserSerializer
+from rest_framework.filters import SearchFilter
 
-from rest_framework.authentication import authenticate
+from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.exceptions import AuthenticationFailed
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ["username", "email"]
 
     def get_serializer_class(self):
-        if self.request.method == 'POST' or self.request.method == 'PUT':
+        if self.request.method in ['POST', 'PUT', 'PATCH']:
             return UserCreateSerializer
         return UserSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "login"]:
+            return [AllowAny()]
+        if self.action in ["list", "retrieve", "me"]:
+            return [IsAuthenticated()]
+        return [IsAuthenticated()]
     
 
     @action(methods=["POST"], detail=False)
