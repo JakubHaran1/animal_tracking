08bf7722fffbb738104a80cc8eff1c4b20d38539
PawelSala <pawel.sala01@wp.pl>
2026-05-05 01:38:34 +0200

    backend: update Observation serializer fields and validation

--- backend/api/serializers/ObservationSerializers.py
+++ backend/api/serializers/ObservationSerializers.py
@@ -23,7 +23,7 @@ class UserSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email','observations']
+        fields = ['id', 'username', 'email', 'avatar', 'observations']
 
 
 class UserCreateSerializer(ModelSerializer):
